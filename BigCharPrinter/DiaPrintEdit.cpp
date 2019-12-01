4e30608fcaf428cb9b875c7cb23f5b7d4435f836
lvy-max <58125217+lvy-max@users.noreply.github.com>
2019-12-01 14:04:21 +0800

    LVY

--- BigCharPrinter/DiaPrintEdit.cpp
+++ BigCharPrinter/DiaPrintEdit.cpp
@@ -28,7 +28,14 @@ void CDiaPrintEdit::DoDataExchange(CDataExchange* pDX)
 
 
 BEGIN_MESSAGE_MAP(CDiaPrintEdit, CDialogEx)
+	ON_LBN_SELCHANGE(IDC_LIST10, &CDiaPrintEdit::OnLbnSelchangeList10)
 END_MESSAGE_MAP()
 
 
 // CDiaPrintEdit 消息处理程序
+
+
+void CDiaPrintEdit::OnLbnSelchangeList10()
+{
+	// TODO: 在此添加控件通知处理程序代码
+}
